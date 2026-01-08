73455382f830a348a93ce334b12b5fc2fd22628d
Johanna Okas <johanna.okas@techno.ee>
2026-01-08 15:09:52 +0200

    First commit

---
+++ JO_esimene_projekt/JO_Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JO_esimene_projekt
+{
+    public partial class JO_Form1 : Form
+    {
+        public JO_Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
